b40d4a845b9918e45f18bc3d11389a13a4836378
Leebam0108 <161020444+Leebam0108@users.noreply.github.com>
2024-02-28 18:36:58 +0900

    Add files via upload

--- 03_myalbum.py
+++ 03_myalbum.py
@@ -1,38 +1,38 @@
 import streamlit as st
 
 st.title('추억이 쌓이는 미니앨범')
-st.markdown('**사진**을 하나씩 추가해서 **:blue[추억]** 만들기 :sunglasses:')
+st.markdown('**:blue[추억의] 사진**을 만들어 보아요 :sunglasses:')
 
 album = [
     {
         "제목" : "2017년 우리 셋만.....",
         "설명" : "스케일에 감탄 한 호텔  ",
-        "image_url" : "https://github.com/Leebam0108/leebam/blob/main/images/img11.jpg"
-    },
-    {
-        "제목" : "비자나무",
-        "설명" : "비자나무 보며 힐링",
-        "image_url" : "https://github.com/Leebam0108/leebam/blob/main/images/img2.jpg"
-    },
-    {
-        "제목" : "절친과 함께",
-        "설명" : "그냥 떠난 여행",
-        "image_url" : "https://github.com/Leebam0108/leebam/blob/main/images/img3.jpg"
-    },
-    {
-        "제목" : "연말명동",
-        "설명" : "연말에만 볼수 있는 곳 ",
-        "image_url" : "https://github.com/Leebam0108/leebam/blob/main/images/img4.jpg"
+        "image_url" : "./images/img1.jpg"
     },
     {
         "제목" : "2023년 마지막 해",
         "설명" : "2024년 행복의 기도를....",
-        "image_url" : "https://github.com/Leebam0108/leebam/blob/main/images/img5.jpg"
+        "image_url" : "./images/img5.jpg"
+    },
+    {
+        "제목" : "연말명동",
+        "설명" : "연말에만 볼수 있는 곳 ",
+        "image_url" : "./images/img3.jpg"
+    },
+    {
+        "제목" : "절친과 함께",
+        "설명" : "행복했던 여행",
+        "image_url" : "./images/img4.jpg"
+    },
+    {
+        "제목" : "힐링 나무",
+        "설명" : "비자나무 보며 힐링",
+        "image_url" : "./images/img2.jpg"
     },
     {
         "제목" : "우리 셋째",
         "설명" : "4살 축하해",
-        "image_url" : "https://github.com/Leebam0108/leebam/blob/main/images/img6.jpg"
+        "image_url" : "./images/img6.jpg"
     }
 ]
 
