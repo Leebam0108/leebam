abbc4a35bfbe597e98c6ab26af219cdfd601d738
Leebam0108 <161020444+Leebam0108@users.noreply.github.com>
2024-02-28 19:13:32 +0900

    Add files via upload

--- 07_traffic.py
+++ 07_traffic.py
@@ -10,10 +10,11 @@ st.markdown("---")                                 # 구분선 생성
 raw = pd.read_csv("./data/시군구별 교통사고 통계.csv")
 df = raw.copy()
 city_name= df["시도"].to_list()
+city_name.sort()
 
 # Selectbox 위젯 생성
 sel_city = st.selectbox(label = "시도선택", # 위젯 상단의 레이블 설정
-                           options = set(city_name), index=11)
+                           options = set(city_name), index=4)
 
 dong = df.loc[(df['시도'] == sel_city), '시군구']
 # Selectbox 위젯 생성
